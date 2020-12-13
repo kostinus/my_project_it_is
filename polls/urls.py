d12618a48b0be13e9de47ff8f9d97f443841bc07
Konstantin Gusev <kostinus@gmail.com>
2020-12-13 19:13:18 +0300

    *

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.question_details, name = 'question'),
+    path('choice_made/', views.choice_made, name = 'choice_made'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,17 @@ from .models import Question, Answer
 def index(request):
     all_questions = Question.objects.filter(is_active = True)
     return render(request, 'index.html', {'all_questions': all_questions})
+
+def question_details(request, question_id):
+    question = Question.objects.get(pk = question_id)
+    return render(request, 'question_details.html', {'question' : question})
+
+def choice_made(request):
+    answer_id = request.GET.get('answer')
+    answer = Answer.objects.get(pk = answer_id)
+    answer.votes += 1
+    answer.save()
+    return HttpResponse("Выбор сделан!")
+
+def test(request):
+    return HttpResponse("Сам ты тест")
